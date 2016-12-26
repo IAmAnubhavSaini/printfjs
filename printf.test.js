74a6a4623f2f3352c150975002122dfb8139781f
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-26 21:22:51 +0530

    Tests via matchstrike.js

---
+++ printf.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var tests = [
+  {
+    toTest: sprintf('%%'),
+    testedFor: '%',
+    description: 'sprintf escapes % with %'
+  },
+  {
+    toTest: sprintf('%d'),
+    testedFor: 'NaN',
+    description: 'sprintf returns empty string'
+  },
+  {
+    toTest: sprintf('%d', 10),
+    testedFor: '10',
+    description: 'sprintf returns 10'
+  },
+  {
+    toTest: sprintf('The percentage is %d%%.', 10),
+    testedFor: 'The percentage is 10%.',
+    description: 'sprintf returns correct string'
+  }
+];
+
+strike(tests, 'printf.test.js');
