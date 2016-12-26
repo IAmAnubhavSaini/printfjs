499ecfaf07703dee41a88a53846e1afe39b8a520
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-26 19:23:13 +0530

    Version 1.0.0

---
+++ printf.js
@@ -0,0 +1,26 @@
+var sprintf = function (fmt) {
+  if(fmt.length <= 0) {
+    return ''
+  }
+  var args = Array.from(arguments)
+  var output = []
+  var currArg = 1
+  for(var i = 0; i < fmt.length; i++) {
+  	while(fmt[i] !== '%' && i < fmt.length) {
+  	  output.push(fmt[i])
+      i++
+    }
+    if(i < fmt.length && fmt[i++] === '%') {
+      switch(fmt[i]) {
+      case 'd':
+        output.push(Number(args[currArg]))
+        currArg++
+        break
+      case '%':
+		output.push('%')
+		break
+      }
+    }
+  }
+  return output.join('')
+}
